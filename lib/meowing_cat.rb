9fcad2233620f0283d16486362d5f50749897ada
Michael Wnek <michaelwnek1@gmail.com>
2019-04-02 21:17:55 +0000

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,16 @@
-## code your solution here. 
+class Cat
+
+attr_accessor = :name, :meow
+
+  def initialize(:name, :meow)
+    @name = name
+    @meow = meow
+  end
+  
+  def meow = (meow)
+    puts "meow"
+
+  end
+  
+
+end
